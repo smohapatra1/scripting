c6b78f3509726fb6cbe830e782b8bf1eb58ad5bf
smohapatra1 <samarendra.mohapatra121@gmail.com>
2019-01-06 22:41:40 -0800

    Adding day3 of practice - tuples

---
+++ python/practice/day3/all_lists.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#Do the list 
+
+x = [ "a", "b" , "c" ]
+print (len(x))
+x.append("d")
+print (x)
+x.insert(1, "e")
+print (x)
+x.reverse()
+print (x)
+x.remove("c")
+print (x)
+del x
+print (x)
