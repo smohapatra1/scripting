68e2dfbdf47f7340db9acdb0b5e64fbca200fd67
smohapatra1 <samarendra.mohapatra121@gmail.com>
2019-11-26 08:18:46 -0600

    how to install ansible

--- ansible/how_to_install_ansible.py
+++ ansible/how_to_install_ansible.py
@@ -1,3 +1,12 @@
 #!/bin/python
 #How to install and configure Ansible
+* sudo  easy_install pip
+* sudo pip install pip
 
+####################
+#Ubuntu 18.04 :- 
+####################
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
