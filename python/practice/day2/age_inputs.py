2b8b4c5e08dcd0e9ab7d600a6dd8b7d66b00a082
smohapatra1 <samarendra.mohapatra121@gmail.com>
2019-11-28 01:41:55 -0600

    Added Input name and age

---
+++ python/practice/day2/age_inputs.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+a = str(raw_input("Enter your name : "))
+b= int(raw_input("Enter your age : "))        
+print ("My name is %s and age is %d" %(a, b))   
+left = 100 - b
+print ("%d year left to be 100 years old" % left)
