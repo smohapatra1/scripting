31c02f805febbf4118d8d548b4246b65affd8062
smohapatra1 <samarendra.mohapatra121@gmail.com>
2019-11-30 09:57:58 -0600

    adding dictionaries code

---
+++ python/practice/day4/dictonaties.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#Use of Dictionaries 
+fruits = {"Mango":10 , "Apple":20 , "Grapes":30}
+print fruits["Mango"]
+#Assign a new value
+fruits["Apple"] = 50
+print (fruits)
+#Remove a values 
+del fruits["Apple"]
+fruits["Mango"] = 100
+print fruits
