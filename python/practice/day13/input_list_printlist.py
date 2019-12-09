3bea23ca5ed9964c9d2140fc9690dafdae287aea
smohapatra1 <samarendra.mohapatra121@gmail.com>
2019-12-09 14:54:48 -0600

    added string operators and list

---
+++ python/practice/day13/input_list_printlist.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#Input multiple words in , and print in list and tuples
+val=raw_input("Enter values : ")
+val1=val.split(",")
+print (list(val1))
+print (tuple(val1))
---
+++ python/practice/day13/string_operatios.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#String operations
+string1 = "Hello Samar!"
+string2 = "Welcome!"
+print (string1 +"\n" + " " + string2)
