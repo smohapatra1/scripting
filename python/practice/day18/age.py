98e5fc8c86e7fcfe1494ce83c5ea59b2b8cf96e1
smohapatra1 <samarendra.mohapatra121@gmail.com>
2019-12-14 09:57:20 -0600

    Some ptactice from edx

---
+++ python/practice/day18/age.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#Write a program that asks the user to enter their age in years as input (assume that the user enters a positive integer) and calculates and prints how old the user is in terms of days. Assume that there are 365 days in a year. For example: if the user enters
+age = int(raw_input("Enter your age :"))
+Ndays=int(365)
+print ("You are %s days old" %(Ndays * age)) 
+
---
+++ python/practice/day18/age2.py
@@ -0,0 +1,4 @@
+# Type your code here
+age=int(input("Enter your Age : "))
+days=int(365)
+print ('You are ' +str(age * days) + ' days old')
