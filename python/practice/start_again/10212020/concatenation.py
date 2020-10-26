ed6babaf90a22d5056a44a2b7ca3b48e672c8ee8
smohapatra1 <samarendra.mohapatra121@gmail.com>
2020-10-26 04:55:48 -0700

    Starting again from formating

---
+++ python/practice/start_again/10212020/concatenation.py
@@ -0,0 +1,15 @@
+#Concatinate/Replace
+#Replace S with P
+a = "Sam"
+b=a[1:]
+c = "P"
+x= c + b
+print (x)
+y = x + " !! Welcome to the club"
+print (y)
+#Upper and Lowecase
+print(y.upper())
+print(y.lower())
+print(y.split())
+#Spliting a special chars
+print(y.split("Pam"))
