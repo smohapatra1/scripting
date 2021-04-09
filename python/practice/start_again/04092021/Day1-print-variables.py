21b85dfdfedf5aff0ee3b4cf0bc3d7f2eb0f399b
smohapatra1 <samarendra.mohapatra121@gmail.com>
2021-04-09 05:14:43 -0700

    Starting day1 of coding - 100 days of code

---
+++ python/practice/start_again/04092021/Day1-print-variables.py
@@ -0,0 +1,7 @@
+#Work on print, input and variables 
+
+#print 
+print ("My name is" + "Samarendra Mohapatra")
+print ("Welcome " + input("Whats your name : "))
+name=input("What is your name ? ")
+print ("My name is : " + name)
---
+++ python/practice/start_again/04092021/exercise3.py
@@ -0,0 +1,3 @@
+def all_abord(a, *args, **kw):
+    print (a, args, kw)
+all_abord(4,7,3,0, x=10, y=64)
