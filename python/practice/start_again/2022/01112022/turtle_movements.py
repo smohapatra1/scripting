38305d9f8edf7a73eb6033dc3622ee4175617ca0
smohapatra1 <samarendra.mohapatra121@gmail.com>
2022-01-23 23:26:17 -0800

    Turtle Movements

---
+++ python/practice/start_again/2022/01112022/turtle_movements.py
@@ -0,0 +1,11 @@
+#Turtule movements
+import turtle
+turtle.distance=90
+turtle.bk(turtle.distance)
+turtle.lt(turtle.distance)
+turtle.fd(turtle.distance)
+turtle.rt(turtle.distance)
+turtle.fd(turtle.distance)
+turtle.rt(turtle.distance)
+turtle.fd(turtle.distance)
+turtle.mainloop()
