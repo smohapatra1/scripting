e249ec9ec38410d3ab3dcdb0d3c646d6ed41f665
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-07-18 21:16:56 -0700

    added everything in one script

--- python/practice/start_again/2023/07112023/contains_duplicate.py
+++ python/practice/start_again/2023/07112023/contains_duplicate.py
@@ -1,18 +1,38 @@
 #https://leetcode.com/problems/contains-duplicate/
 #Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+#Check Duplicate 
 
-def containsDuplicate(self, nums: List[int]) -> bool:
-    s=set(nums)
-    if len(s) == len(nums):
-        return False
-    else:
-        return True 
-    
+#BFM Method 
+# def containsDuplicate(nums):
+#     for i in range(n):
+#         for j in range (i+1, n ):
+#             if nums[i] == nums[j]:
+#                 return True
+#     return False
+
+# Sorting Method
+# def containsDuplicate(nums):
+#     nums.sort()
+#     for i in range (1,n):
+#         if nums[i] == nums[i-1]:
+#             return True
+#     return False 
+
+#Set Method 
+def containsDuplicate(nums):
+    num=set()
+    for i in nums:
+        if i in num:
+            return True
+        num.add(i)
+    return False        
+
 if __name__ == "__main__":
-    List=[1,2,3,4]
-    if containsDuplicate(List):
-        print ("Duplicate Exist")
+    nums=[1,2,3,4]
+    n=len(nums)
+    if containsDuplicate(nums):
+        print ("Duplicate exist")
     else:
         print ("Duplicate doesn't exist")
         
