952c12a02e05bcb43e99da5ade5ec46e57d948bd
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-08-24 22:41:46 -0700

    reorder list

---
+++ python/practice/start_again/2023/08242023/reorder_list.py
@@ -0,0 +1,55 @@
+# 143. Reorder List
+# Medium
+
+# You are given the head of a singly linked-list. The list can be represented as:
+
+# L0 → L1 → … → Ln - 1 → Ln
+# Reorder the list to be on the following form:
+
+# L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+# You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4]
+# Output: [1,4,2,3]
+# Example 2:
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [1,5,2,4,3]
+
+
+def reorder(head):
+    arr=[]
+    curr = head
+    length = 0
+    while curr:
+        arr.append(curr)
+        curr = curr.next
+        length = length +1 
+    left = 0 
+    right = length -1
+    last=head 
+    while left < right:
+        arr[left].next = arr[right]
+        left +=1
+        if left == right:
+            last=arr[right]
+            break
+        arr[right].next = arr[left]
+        right -=1
+        last = arr[left]
+    if last:
+        last.next=None
+ 
+
+
+
+
+if __name__ == "__main__":
+    head = [1,2,3,4]
+    print ("{}".format(reorder(head)))
