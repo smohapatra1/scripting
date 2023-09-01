700c2d9c40d7b2c77e5128d9f3c85b901ff022b9
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-09-01 15:19:37 -0700

    Fixed a mistake

--- python/practice/start_again/2023/09012023/max_depth_binary_tree.py
+++ python/practice/start_again/2023/09012023/max_depth_binary_tree.py
@@ -23,7 +23,7 @@ def maxDepth(root):
     def dfs(root, depth):
         if not root:
             return depth
-        return (max(dfs(root.left, depth+1), dfs(root.left, depth+1)))
+        return (max(dfs(root.left, depth+1), dfs(root.right, depth+1)))
     return dfs(root, 0)
 
 
