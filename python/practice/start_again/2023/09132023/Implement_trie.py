1837de972d3a08dd5f7168f38b3c78bfa9bb325f
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-09-13 23:55:47 -0700

    Added implementing tries

---
+++ python/practice/start_again/2023/09132023/Implement_trie.py
@@ -0,0 +1,58 @@
+# 208. Implement Trie (Prefix Tree)
+# Medium
+# 10.8K
+# 120
+# Companies
+# A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+# Implement the Trie class:
+
+# Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+# boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+# Example 1:
+
+# Input
+# ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+# [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+# Output
+# [null, null, true, false, true, null, true]
+
+# Explanation
+# Trie trie = new Trie();
+# trie.insert("apple");
+# trie.search("apple");   // return True
+# trie.search("app");     // return False
+# trie.startsWith("app"); // return True
+# trie.insert("app");
+# trie.search("app");     // return True
+
+
+class Tree:
+    def __init__(self):
+        self.root={}
+    def insert(self, word:str) -> None:
+        curr=self.root
+        for letter in word:
+            if letter not in curr:
+                curr[letter]={}
+            curr=curr[letter]
+        curr['*']=''
+    def search(self,word:str) -> bool:
+        curr=self.root
+        for letter in word:
+            if letter not in curr:
+                return False
+            curr=curr[letter]
+        return '*' in curr
+    def startWith(self, prefix: str) -> bool:
+        curr=self.root
+        for letter in prefix:
+            if letter not in curr:
+                return False
+            curr=curr[letter]
+        return True
+
