d2c0569712d1d224640d35e4fbea3a731730e412
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-10-09 22:39:25 -0700

    find max Subarray

---
+++ python/practice/start_again/2023/10092023/max_subarray.py
@@ -0,0 +1,56 @@
+# 53. Maximum Subarray
+# Medium
+# 32.2K
+# 1.3K
+# Companies
+# Given an integer array nums, find the 
+# subarray
+#  with the largest sum, and return its sum.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+# Example 3:
+
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+
+from typing import List
+def maxSubarray(nums: List[int]) -> int:
+    #Solutuon-01
+    # currSum=0
+    # maxSum=float('-inf')
+    # for num in nums:
+    #     currSum +=num
+    #     if currSum > maxSum:
+    #         maxSum = currSum
+    #     if currSum < 0:
+    #         currSum =0 
+    # return maxSum
+    #Solution-02
+    maxSum=nums[0]
+    currSum=nums[0]
+    for num in nums[1:]:
+        currSum=max(num, currSum + num )
+        maxSum=max(maxSum, currSum)
+    return maxSum
+
+
+
+
+
+if __name__ == "__main__":
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    print ("{}".format(maxSubarray(nums)))
+
