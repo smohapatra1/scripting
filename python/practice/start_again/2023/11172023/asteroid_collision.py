716ee18e06f6b1d2b9eb671b388f7fe6e57fb7e9
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-11-17 16:20:15 -0800

    Stack: Asteroid collision

---
+++ python/practice/start_again/2023/11172023/asteroid_collision.py
@@ -0,0 +1,51 @@
+# #   https://leetcode.com/problems/asteroid-collision/
+# 735. Asteroid Collision
+# Medium
+# 7.3K
+# 838
+# Companies
+# We are given an array asteroids of integers representing asteroids in a row.
+
+# For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+# Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+ 
+
+# Example 1:
+
+# Input: asteroids = [5,10,-5]
+# Output: [5,10]
+# Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+# Example 2:
+
+# Input: asteroids = [8,-8]
+# Output: []
+# Explanation: The 8 and -8 collide exploding each other.
+# Example 3:
+
+# Input: asteroids = [10,2,-5]
+# Output: [10]
+# Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ 
+from typing import List
+def asteroidCollison(ast: List[int]) -> List[int]:
+    stack =[]
+    for i in ast:
+        while stack and stack[-1] > 0 and i < 0:
+            if stack[-1] + i < 0:
+                stack.pop()
+            elif stack[-1] + i > 0:
+               break
+            else:
+               stack.pop()
+               break
+        else:
+           stack.append(i)
+    return stack
+           
+
+
+if __name__ == "__main__":
+    ast=[5,10,-5]
+    print ("{}".format(asteroidCollison(ast)))
