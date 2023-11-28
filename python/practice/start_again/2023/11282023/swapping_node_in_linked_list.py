17a7ab9ba57f80ec6d249fc4cba3c69b5f2dc9a5
smohapatra1 <samarendra.mohapatra121@gmail.com>
2023-11-28 15:12:25 -0800

    Linked List: Swapping nodes in a linkedlist

---
+++ python/practice/start_again/2023/11282023/swapping_node_in_linked_list.py
@@ -0,0 +1,37 @@
+# #   https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+# 1721. Swapping Nodes in a Linked List
+# Medium
+# 5.1K
+# 165
+# Companies
+# You are given the head of a linked list, and an integer k.
+
+# Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [1,4,3,2,5]
+# Example 2:
+
+# Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+# Output: [7,9,6,6,8,7,3,0,9,5]
+
+def SwapNodes(head: ListNode, k: int) -> ListNode:
+    left = right = head 
+    for _ in range(k-1):
+        left = left.next
+    tail = left 
+    while tail.next:
+        right, tail = right.next, tail.next
+    left.val, right.val = right.val, left.val
+    return head
+
+
+if __name__ == "__main__":
+    head = [1,2,3,4,5]
+    k = 2
+    print ("{}".format(SwapNodes(head, k)))
