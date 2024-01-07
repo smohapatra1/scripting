b331b7fbd2bc8e4f15e588c7a5b37ea98e64316a
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-01-07 12:02:32 -0800

    2D-DP: Stone Game II

---
+++ python/practice/start_again/2024/01062024/stone_game_II.py
@@ -0,0 +1,44 @@
+# #   https://leetcode.com/problems/stone-game-ii/
+
+# 1140. Stone Game II
+# Medium
+# 2.8K
+# 610
+# Companies
+# Alice and Bob continue their games with piles of stones.  There are a number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].  The objective of the game is to end with the most stones. 
+
+# Alice and Bob take turns, with Alice starting first.  Initially, M = 1.
+
+# On each player's turn, that player can take all the stones in the first X remaining piles, where 1 <= X <= 2M.  Then, we set M = max(M, X).
+
+# The game continues until all the stones have been taken.
+
+# Assuming Alice and Bob play optimally, return the maximum number of stones Alice can get.
+
+ 
+
+# Example 1:
+
+# Input: piles = [2,7,9,4,4]
+# Output: 10
+# Explanation:  If Alice takes one pile at the beginning, Bob takes two piles, then Alice takes 2 piles again. Alice can get 2 + 4 + 4 = 10 piles in total. If Alice takes two piles at the beginning, then Bob can take all three piles left. In this case, Alice get 2 + 7 = 9 piles in total. So we return 10 since it's larger. 
+# Example 2:
+
+# Input: piles = [1,2,3,4,5,100]
+# Output: 104
+
+
+from typing import List
+from functools import lru_cache
+def StoneGameII(piles: List[int]) -> int:
+    @lru_cache
+    def fn(i,m):
+        if len(piles) - i <=2*m:
+            return sum(piles[i:])
+        return max(sum(piles[i:i+j])-fn(i+j, max(m,j))for j in range(1, 2*m+1))
+    return (fn(0,1) + sum(piles))//2
+
+
+if __name__ == "__main__":
+    piles = [2,7,9,4,4]
+    print ("{}".format(StoneGameII(piles)))
