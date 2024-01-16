f06867fa720f73e2d54d37ef15bfdb47aa92fada
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-01-15 16:11:32 -0800

    Bit Manupulations: Add to array from integer

---
+++ python/practice/start_again/2024/01182024/add_to_array_from_integer.py
@@ -0,0 +1,44 @@
+# #   https://leetcode.com/problems/add-to-array-form-of-integer/description/
+
+# 989. Add to Array-Form of Integer
+# Easy
+
+# Topics
+# Companies
+# The array-form of an integer num is an array representing its digits in left to right order.
+
+# For example, for num = 1321, the array form is [1,3,2,1].
+# Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+ 
+
+# Example 1:
+
+# Input: num = [1,2,0,0], k = 34
+# Output: [1,2,3,4]
+# Explanation: 1200 + 34 = 1234
+# Example 2:
+
+# Input: num = [2,7,4], k = 181
+# Output: [4,5,5]
+# Explanation: 274 + 181 = 455
+# Example 3:
+
+# Input: num = [2,1,5], k = 806
+# Output: [1,0,2,1]
+# Explanation: 215 + 806 = 1021
+
+from typing import List
+def AddArray(num : List[int], k: int )-> List[int]:
+    for i in range(len(num) -1, -1, -1 ):
+        k, num[i] = divmod(num[i] +k , 10)
+    while k:
+        k, a = divmod(k, 10)
+        num = [a] + num 
+    return num
+        
+
+if __name__ == "__main__":
+    num = [1,2,0,1]
+    k = 34
+    print ("{}".format(AddArray(num, k)))
