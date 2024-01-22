bb4f9ff51812dffbd39bbd860db3c55bfcdbb8c1
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-01-22 11:51:09 -0800

    Itertools: Maximize it

---
+++ python/practice/start_again/2024/01262024/maximize_it.py
@@ -0,0 +1,50 @@
+# #   https://www.hackerrank.com/challenges/maximize-it/problem?isFullScreen=false
+
+# You are given a function . You are also given  lists. The  list consists of  elements.
+
+# You have to pick one element from each list so that the value from the equation below is maximized:
+
+# %
+
+#  denotes the element picked from the  list . Find the maximized value  obtained.
+
+#  denotes the modulo operator.
+
+# Note that you need to take exactly one element from each list, not necessarily the largest element. You add the squares of the chosen elements and perform the modulo operation. The maximum value that you can obtain, will be the answer to the problem.
+
+# Input Format
+
+# The first line contains  space separated integers  and .
+# The next  lines each contains an integer , denoting the number of elements in the  list, followed by  space separated integers denoting the elements in the list.
+
+# Constraints
+
+
+
+
+
+# Output Format
+
+# Output a single integer denoting the value .
+
+# Sample Input
+
+# 3 1000
+# 2 5 4
+# 3 7 8 9 
+# 5 5 7 8 9 10 
+# Sample Output
+
+# 206
+# Explanation
+
+# Picking  from the st list,  from the nd list and  from the rd list gives the maximum  value equal to % = .
+
+
+from itertools import product
+
+if __name__ == "__main__":
+    K,M=map(int, input().split())
+    N=(list(map(int, input().split()))[1:] for _ in range(K))
+    result=map(lambda x: sum(i**2 for i in x)%M, product(*N))
+    print (max(result))
