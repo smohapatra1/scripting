e4573882ba8ca8628c361d7ebd5cd014f91ca80e
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-02-24 09:31:47 -0800

    Intv-prep-Monthly:-  Week1-Day1 - Time converstion fixed

--- python/practice/start_again/2024/02282024/time_conversion.py
+++ python/practice/start_again/2024/02282024/time_conversion.py
@@ -57,14 +57,15 @@ import sys
 def timeConversion(s):
     # Write your code here
     time=s.split(':')
+    time = s.split(":")
     if s[-2:] == "PM":
         if time[0] != "12":
             time[0] = str(int(time[0])+12)
-        else:
-            if time[0] == "12" :
-                time[0] = "00"
-    new_time=':'.join(time)
-    return str(new_time[:-2])
+    else:
+        if time[0] == "12":
+            time[0] = "00"
+    ntime = ':'.join(time)
+    return str(ntime[:-2])
     
             
 if __name__ == '__main__':
