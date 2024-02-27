2c90ea4a845968a49019fbafa737b4f16bca7442
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-02-26 17:14:41 -0800

    Intv-prep-Monthly:-  Week1-Day4 - Mock Test:-  Flipping the matrix

---
+++ python/practice/start_again/2024/03032024/flipping_the_matrix.py
@@ -0,0 +1,21 @@
+#   https://www.hackerrank.com/test/flgjjnhfm7i/questions/di1dm3kpigj
+
+
+def flippingMatrix(matrix):
+    n=len(matrix)
+    total=0
+    for i in range(n//2):
+        for j in range(n//2):
+            total +=max(matrix[i][j], matrix[i][n-j-1],matrix[n-i-1][j],matrix[n-i-1][n-j-1])
+    return total
+
+if __name__ == "__main__":
+    q=int(input().strip())
+    for _ in range(q):
+        n=int(input().strip())  
+        matrix=[]
+        for _ in range(2*n):
+            row=list(map(int, input().strip().split()))
+            matrix.append(row)
+        result=flippingMatrix(matrix)
+        print (result)
