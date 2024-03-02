3fd2e160f031ad5c4d3402dc008a6842e37c2902
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-03-02 09:19:18 -0800

    Intv-prep-Monthly:-  Week3-Day10 - Climbing leaderboard

---
+++ python/practice/start_again/2024/03092024/climbing_leaderboard.py
@@ -0,0 +1,89 @@
+# #   https://www.hackerrank.com/challenges/one-month-preparation-kit-climbing-the-leaderboard/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-three
+# An arcade game player wants to climb to the top of the leaderboard and track their ranking. The game uses Dense Ranking, so its leaderboard works like this:
+
+# The player with the highest score is ranked number  on the leaderboard.
+# Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
+# Example
+
+
+
+# The ranked players will have ranks , , , and , respectively. If the player's scores are ,  and , their rankings after each game are ,  and . Return .
+
+# Function Description
+
+# Complete the climbingLeaderboard function in the editor below.
+
+# climbingLeaderboard has the following parameter(s):
+
+# int ranked[n]: the leaderboard scores
+# int player[m]: the player's scores
+# Returns
+
+# int[m]: the player's rank after each new score
+# Input Format
+
+# The first line contains an integer , the number of players on the leaderboard.
+# The next line contains  space-separated integers , the leaderboard scores in decreasing order.
+# The next line contains an integer, , the number games the player plays.
+# The last line contains  space-separated integers , the game scores.
+
+# Constraints
+
+#  for 
+#  for 
+# The existing leaderboard, , is in descending order.
+# The player's scores, , are in ascending order.
+# Subtask
+
+# For  of the maximum score:
+
+# Sample Input 1
+
+# CopyDownload
+# 7
+# 100 100 50 40 40 20 10
+# 4
+# 5 25 50 120
+# Sample Output 1
+
+# 6
+# 4
+# 2
+# 1
+
+# Sample Input 2
+
+# CopyDownload
+# 6
+# 100 90 90 80 75 60
+# 5
+# 50 65 77 90 102
+# Sample Output 2
+
+# 6
+# 5
+# 4
+# 2
+# 1
+
+
+def climbingLeaderboard(ranked, player):
+    ranked=list(sorted(set(ranked)))
+    init_rank=len(ranked)+1
+    result=[]
+    for p in player:
+        while init_rank >1 and ranked[0] <=p:
+            init_rank -=1
+            ranked.pop(0)
+        result.append(init_rank)
+    return result
+
+
+
+if __name__ == "__main__":
+    ranked_count=int(input().rstrip())
+    ranked=list(map(int, input().rstrip().split()))
+    player_count=int(input().rstrip())
+    player=list(map(int, input().rstrip().split()))
+    result=climbingLeaderboard(ranked, player)
+    print (result)
