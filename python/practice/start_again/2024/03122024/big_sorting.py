c24df6ca8af822f489dd7052cffb4c999077258a
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-03-05 12:26:57 -0800

    Interview-prep-Monthly:- Week3-Day13 - Mock-Test: - Big sorting

---
+++ python/practice/start_again/2024/03122024/big_sorting.py
@@ -0,0 +1,40 @@
+#   https://www.hackerrank.com/test/4j8jqbol4g4/questions/a4rcfj3k2bk
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'bigSorting' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY unsorted as parameter.
+#
+
+def bigSorting(unsorted):
+    # Write your code here
+    return sorted(sorted(unsorted), key=len)
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    # fptr.write('\n'.join(result))
+    # fptr.write('\n')
+
+    # fptr.close()
