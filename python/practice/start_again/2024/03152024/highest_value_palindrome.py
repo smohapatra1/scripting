d1b13ed4b87098e93cd2c4823bf7d19e6305462d
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-03-08 15:55:37 -0800

    Interview-prep-Monthly:- Week4-Day16 - Highest valud of palindrome

---
+++ python/practice/start_again/2024/03152024/highest_value_palindrome.py
@@ -0,0 +1,130 @@
+# #   https://www.hackerrank.com/challenges/one-month-preparation-kit-richie-rich/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-four
+#     Palindromes are strings that read the same from the left or right, for example madam or 0110.
+
+# You will be given a string representation of a number and a maximum number of changes you can make. Alter the string, one digit at a time, to create the string representation of the largest number possible given the limit to the number of changes. The length of the string may not be altered, so you must consider 's left of all higher digits in your tests. For example  is valid,  is not.
+
+# Given a string representing the starting number, and a maximum number of changes allowed, create the largest palindromic string of digits possible or the string '-1' if it is not possible to create a palindrome under the contstraints.
+
+# Example
+
+
+# Make  replacements to get .
+
+
+
+# Make  replacement to get .
+
+# Function Description
+
+# Complete the highestValuePalindrome function in the editor below.
+
+# highestValuePalindrome has the following parameter(s):
+
+# string s: a string representation of an integer
+# int n: the length of the integer string
+# int k: the maximum number of changes allowed
+# Returns
+
+# string: a string representation of the highest value achievable or -1
+# Input Format
+
+# The first line contains two space-separated integers,  and , the number of digits in the number and the maximum number of changes allowed.
+# The second line contains an -digit string of numbers.
+
+# Constraints
+
+# Each character  in the number is an integer where .
+# Output Format
+
+# Sample Input 0
+
+# STDIN   Function
+# -----   --------
+# 4 1     n = 4, k = 1
+# 3943    s = '3943'
+# Sample Output 0
+
+# 3993
+# Sample Input 1
+
+# 6 3
+# 092282
+# Sample Output 1
+
+# 992299
+# Sample Input 2
+
+# 4 1
+# 0011
+# Sample Output 2
+
+# -1
+# Explanation
+
+# Sample 0
+
+# There are two ways to make  a palindrome by changing no more than  digits:
+
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'highestValuePalindrome' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER n
+#  3. INTEGER k
+#
+
+def highestValuePalindrome(s, n, k):
+    # Write your code here
+    arr, diff = [*s], dict()
+    for i in range(n//2):
+        if k < 0 :
+            break
+        if arr[i] != arr[-1 - i ]:
+            m=max(s[i], s[-1 -i ])
+            arr[i], arr[-1 -i ] = m, m 
+            k, diff[i] = k-1, True 
+    if k < 0:
+        return '-1'
+    if k > 0:
+        for i in range(n//2):
+            if k == 0:
+                break
+            if arr[i] != '9':
+                if i in diff:
+                    arr[i], arr[n-1 - i ] = '9', '9'
+                    k -= 1
+                elif k > 1 :
+                    arr[i], arr[n-1 -i ] = '9', '9'
+                    k -=2
+    if k >=1 and n% 2 ==1 :
+        arr[n//2]  = '9'
+    return ''.join(arr)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    s = input()
+
+    result = highestValuePalindrome(s, n, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
