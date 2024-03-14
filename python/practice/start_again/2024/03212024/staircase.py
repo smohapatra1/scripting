4a7d202d1f64925e0b80836f626992621108b511
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-03-14 13:30:25 -0700

    Staircases

---
+++ python/practice/start_again/2024/03212024/staircase.py
@@ -0,0 +1,65 @@
+# #   https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+# Staircase detail
+
+# This is a staircase of size :
+
+#    #
+#   ##
+#  ###
+# ####
+# Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+# Write a program that prints a staircase of size .
+
+# Function Description
+
+# Complete the staircase function in the editor below.
+
+# staircase has the following parameter(s):
+
+# int n: an integer
+# Print
+
+# Print a staircase as described above.
+
+# Input Format
+
+# A single integer, , denoting the size of the staircase.
+
+# Constraints
+
+#  .
+
+# Output Format
+
+# Print a staircase of size  using # symbols and spaces.
+
+# Note: The last line must have  spaces in it.
+
+# Sample Input
+
+# 6 
+# Sample Output
+
+#      #
+#     ##
+#    ###
+#   ####
+#  #####
+# ######
+# Explanation
+
+# The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+
+
+def staircase(n):
+    for i in range(1, n+1):
+        symbol=i*'#'
+        print (symbol.rjust(n))
+        # print (' '* (n-i) + '#' * i )
+
+
+if __name__ == "__main__":
+    n=int(input().strip())
+    result=staircase(n)
+    
