2d160195b6d6fbaddbb1c96e5eb511e74ff2c09a
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-03-28 11:21:53 -0700

    Algorithms - Sherlock and squares

---
+++ python/practice/start_again/2024/04042024/sherlock_and_squares.py
@@ -0,0 +1,60 @@
+# #   https://www.hackerrank.com/challenges/sherlock-and-squares/problem?isFullScreen=true
+
+# Watson likes to challenge Sherlock's math ability. He will provide a starting and ending value that describe a range of integers, inclusive of the endpoints. Sherlock must determine the number of square integers within that range.
+
+# Note: A square integer is an integer which is the square of an integer, e.g. .
+
+# Example
+
+
+# There are three square integers in the range:  and . Return .
+
+# Function Description
+
+# Complete the squares function in the editor below. It should return an integer representing the number of square integers in the inclusive range from  to .
+
+# squares has the following parameter(s):
+
+# int a: the lower range boundary
+# int b: the upper range boundary
+# Returns
+
+# int: the number of square integers in the range
+# Input Format
+
+# The first line contains , the number of test cases.
+# Each of the next  lines contains two space-separated integers,  and , the starting and ending integers in the ranges.
+
+# Constraints
+
+
+
+# Sample Input
+
+# 2
+# 3 9
+# 17 24
+# Sample Output
+
+# 2
+# 0
+# Explanation
+
+# Test Case #00: In range ,  and  are the two square integers.
+# Test Case #01: In range , there are no square integers.
+
+import math
+
+def Squares(a,b):
+    res=math.floor(math.sqrt(b)) - math.floor(math.sqrt(a-1))
+    return res
+
+
+if __name__ == "__main__":
+    q = int(input().strip())
+    for q_iter in range(q):
+        q1 = input().rstrip().split()
+        a = int(q1[0])
+        b = int(q1[1])
+        result = Squares(a,b)
+        print (result)
