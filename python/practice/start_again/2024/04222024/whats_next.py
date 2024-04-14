a903c2bc77cd538e4e0689544d9961e350216593
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-04-14 10:56:23 -0700

    Algorithms - Bit Manipulations - What's next

--- python/practice/start_again/2024/04222024/whats_next.py
+++ python/practice/start_again/2024/04222024/whats_next.py
@@ -50,8 +50,6 @@
 
 # Having found , we print its length () as our first line of output, followed by the space-separated elements in  as our second line of output.
 
-
-
 #!/bin/python3
 
 import math
@@ -68,34 +66,42 @@ import sys
 
 def whatsNext(arr):
     # Write your code here
-    l = len(arr)-1
+    l = len(arr) -1
+    # case: l == 0
     if not l:
         if arr[0] == 1:
-            print (2)
-            print (*[1,1])
+            print(2)
+            print(*[1, 1])
         else:
-            print (3)
-            print (*[1,1, arr[0]-1])
-        return 
-    i  = l-1 if l & 1 else l
+            print(3)
+            print(*[1, 1, arr[0]-1])
+        return
+    # find index for least significant set bit
+    i = l-1 if l & 1 else l
+    # case: l == 1
     if l == 1:
-        t = [1, arr[1] + 1, arr[0] -1]
-    elif l -i:
+        t = [1, arr[1] +1, arr[0] -1]
+    # case: last bit group is off
+    elif l - i:
         t = arr[:i-1] + [arr[i-1] -1, 1, arr[-1] + 1, arr[i] -1]
+    # case: last bit group is set
     else:
-         t = arr[:i-1] + [arr[i-1] -1, 1, 1, arr[i] -1]
-    if not t[-1]:t:pop(-1)
+        t = arr[:i-1] + [arr[i-1] -1, 1, 1, arr[i] -1]
+    # handle zeros
+    if not t[-1]: t.pop(-1)
     if t.count(0):
         i_lst = []
-        for i, a in enumerate(t):
-            if not a :
-                t[i-1] +=t[i+1]
+        for i,a in enumerate(t):
+            if not a:
+                #  combine bits around zero
+                t[i-1] += t[i+1]
                 i_lst.append(i)
+        # remove zero and extra bits
         for i in i_lst[-1:-1-len(i_lst):-1]:
             t.pop(i+1)
             t.pop(i)
-    print (len(t))
-    print (*t)
+    print(len(t))
+    print(*t)
 
 if __name__ == '__main__':
     t = int(input().strip())
