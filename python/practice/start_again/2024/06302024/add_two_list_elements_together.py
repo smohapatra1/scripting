094a17802a6f672987248f18ce70b783f7565994
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-06-06 17:06:30 -0700

    adding two list elements together

--- python/practice/start_again/2024/06302024/add_two_list_elements_together.py
+++ python/practice/start_again/2024/06302024/add_two_list_elements_together.py
@@ -1,11 +1,5 @@
 # Add two lists elements together
 
-# def add_list(n, m ):
-#     a = ''.join(n)
-#     b = ''.join(m)
-#     print ('{} {}'.format(a, b))
-
-
 def add_list(n, m ):
     res_list = []
     for i in range(0, len(n)):
@@ -13,8 +7,6 @@ def add_list(n, m ):
     print (res_list)
 
 if __name__ == "__main__":
-    # n = ['P', 'y', 't', 'h', 'o', 'n']
-    # m = ['i' ,'s', ' ', 'g', 'o', 'o', 'd']
     n = [1, 2, 3]
     m = [4, 5, 6]
     res = add_list(n,m)
