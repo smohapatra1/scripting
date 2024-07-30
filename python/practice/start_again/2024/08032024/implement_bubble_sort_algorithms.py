1c812381d4b1fb200e29ae5d5629c6566b33cc14
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-07-30 15:54:07 -0700

    Basic - Implement bubble sort algorithm

---
+++ python/practice/start_again/2024/08032024/implement_bubble_sort_algorithms.py
@@ -0,0 +1,14 @@
+#   How Is a Bubble Sort Algorithm Implemented?
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1 ):
+            if arr[j] > arr[j+1]:
+                arr[j] , arr[j+1] = arr[j+1], arr[j]
+
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    result = bubbleSort(arr)
+    print (result)
