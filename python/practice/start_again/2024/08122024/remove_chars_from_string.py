7229f1b776ffb966db103e625e70d0c28f740c4d
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-08-06 11:21:47 -0700

    Basic - Remove chars from a string

---
+++ python/practice/start_again/2024/08122024/remove_chars_from_string.py
@@ -0,0 +1,9 @@
+#   Python program to remove character from string
+
+def Replace(s):
+    ch = 'O'
+    print (s.replace(ch, ' '))
+
+if __name__ == "__main__":
+    s = 'PYTHON'
+    result = Replace(s)
