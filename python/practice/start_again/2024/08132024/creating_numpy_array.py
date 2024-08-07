5381900a27a854aa6628e335ef74346996c3e19e
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-08-07 10:45:24 -0700

    Basic - Creating numpy array

---
+++ python/practice/start_again/2024/08132024/creating_numpy_array.py
@@ -0,0 +1,9 @@
+#   Creating a NumPy Array in Python
+
+import numpy as np
+def Numpy(arr):
+    print (np.array(arr))
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 5, 6 ]
+    result = Numpy(arr)
