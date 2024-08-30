b93c81d90f64cb78ea275af6e6406d495f4d7d2d
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-08-30 10:21:08 -0700

    Basic - String - Captilize first and last chars in a string

---
+++ python/practice/start_again/2024/09082024/captialize_first_and_last_chars_of_a_string.py
@@ -0,0 +1,16 @@
+#   https://www.geeksforgeeks.org/python-program-to-capitalize-the-first-and-last-character-of-each-word-in-a-string/
+
+# Use title 
+# Use splits 
+# Indexed them with first and last item
+
+def Caps(s):
+    res = ''
+    for i in s.title().split():
+        res += (i[:-1] +i [-1].upper()) + ' '
+    return res
+
+if __name__ == "__main__":
+    s = 'hello world'
+    result = Caps(s)
+    print (result)
