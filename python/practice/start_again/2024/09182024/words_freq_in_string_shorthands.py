96c516bf132aec91a2cf3148d21f21a4d735b7fb
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-09-09 12:16:09 -0700

    Basic - String - Words frequency in string shorthands

--- python/practice/start_again/2024/09182024/words_freq_in_string_shorthands.py
+++ python/practice/start_again/2024/09182024/words_freq_in_string_shorthands.py
@@ -1,9 +1,16 @@
 #   https://www.geeksforgeeks.org/python-words-frequency-in-string-shorthands/?ref=leftbar-rightbar
 
+from collections import Counter
 
 def Freq_in_String(test_str):
-    result = {key: test_str.count(key) for key in test_str.split()}
-    return result
+    # Solution - 01 
+    # result = {key: test_str.count(key) for key in test_str.split()}
+    # return result
+    # Solution - 02 
+    
+    result=Counter(test_str.split())
+    return dict(result)
+
 
 if __name__ == "__main__":
     test_str = 'Gfg is best . Geeks are good and Geeks like Gfg'
