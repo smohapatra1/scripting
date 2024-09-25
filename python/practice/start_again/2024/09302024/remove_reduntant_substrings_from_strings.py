3457013b4a84b49c3fe8f93e1665e95f9c75ec75
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-09-25 06:55:47 -0700

    Basic - String - Remove redundant substrings from strings

---
+++ python/practice/start_again/2024/09302024/remove_reduntant_substrings_from_strings.py
@@ -0,0 +1,14 @@
+#   https://www.geeksforgeeks.org/python-remove-redundant-substrings-from-strings-list/
+
+def Redundant(test_list):
+    test_list.sort(key = len)
+    res = []
+    for idx , val in enumerate(test_list):
+        if val not in ', '.join(test_list[idx +1:]):
+            res.append(val)
+    return res
+
+if __name__ == "__main__":
+    test_list = ["Gfg", "Gfg is best", "Geeks", "Gfg is for Geeks"]
+    result = Redundant(test_list)
+    print (result)
