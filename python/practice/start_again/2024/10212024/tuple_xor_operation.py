3900b34fb1785d3bf89e0ecfa349f7468d58fc5f
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-10-13 10:03:12 -0700

    Basic - Tuples - Tuple XOR operation

---
+++ python/practice/start_again/2024/10212024/tuple_xor_operation.py
@@ -0,0 +1,10 @@
+#   https://www.geeksforgeeks.org/python-tuple-xor-operation/
+
+def XOR(test_tup1, test_tup2):
+    res = tuple(ele1 ^ ele2 for ele1, ele2 in zip(test_tup1, test_tup2))
+    return res
+
+if __name__ == "__main__":
+    test_tup1 = (10, 4, 6, 9) 
+    test_tup2 = (5, 2, 3, 3) 
+    print ("XOR operations = ", XOR(test_tup1, test_tup2))
