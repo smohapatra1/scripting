90caa5497f6c9ff453987086287c356e40aa1d98
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-10-12 23:03:09 -0700

    Basic - Tuples - filter tuples by kth list

--- python/practice/start_again/2024/10192024/filter_tuples_by_kth_elememnt_from_list.py
+++ python/practice/start_again/2024/10192024/filter_tuples_by_kth_elememnt_from_list.py
@@ -1,11 +1,11 @@
 #   https://www.geeksforgeeks.org/python-filter-tuples-by-kth-element-from-list/
 
-def KthList(test_list, K ):
-    check_list = [4, 2, 8, 10]
+def KthList(test_list, K , check_list):
     res = [sub for sub in test_list if sub[K] in check_list]
     return res
 if __name__ == "__main__":
     test_list = [("GFg", 5, 9), ("is", 4, 3), ("best", 10, 29)]
     K = 1
-    result = KthList(test_list, K)
+    check_list = [4, 2, 8, 10]
+    result = KthList(test_list, K, check_list)
     print (result)
