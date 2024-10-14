f66421eacb746db3d10c8ddc4b40a459a3996ec5
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-10-14 11:32:01 -0700

    Basic - Dictionary - Sort python dictonaries by key or value - solution -02

--- python/practice/start_again/2024/10222024/sort_python_dictonaries_by_key_value.py
+++ python/practice/start_again/2024/10222024/sort_python_dictonaries_by_key_value.py
@@ -1,10 +1,17 @@
+# https://www.geeksforgeeks.org/python-sort-python-dictionaries-by-key-or-value/
+from collections import OrderedDict
 
 def SortedD(d):
+
     print ("Current Dictionary    : " + str(d))
-    myKeys = list(d.keys())
-    myKeys.sort()
-    sd = {i: d[i] for i in myKeys}
-    return sd
+    # Solution - 01 
+    # myKeys = list(d.keys())
+    # myKeys.sort()
+    # sd = {i: d[i] for i in myKeys}
+    # return sd
+    # Solution - 02 
+    d1 = OrderedDict(sorted(d.items()))
+    return d1
 
 if __name__ == "__main__":
     # d = {'ravi': 10, 'rajnish': 9, 'sanjeev': 15}
