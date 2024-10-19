ddf9cc10ce7dc29b0eabedd3e46767420a5f9cbf
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-10-19 10:20:41 -0700

    Basic - Dictionary - Removing a key from dictorinary

---
+++ python/practice/start_again/2024/10282024/ways_to_remove_a_key_from_dictionary.py
@@ -0,0 +1,5 @@
+#   https://www.geeksforgeeks.org/python-ways-to-remove-a-key-from-dictionary/
+test_dict = {"Arushi": 22, "Mani": 21, "Haritha": 21}
+print ("Current Dict : ",  str(test_dict))
+del test_dict['Mani']
+print ("Dict after deleting : ",  str(test_dict))
