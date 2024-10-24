763160daefdbeca3442f343800fd159685629247
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-10-24 10:31:09 -0700

    Basic - Dictionary - Convert list of tuples into dictionary

---
+++ python/practice/start_again/2024/11032024/convert_list_of_tuples_into_dictionary.py
@@ -0,0 +1,11 @@
+#   https://www.geeksforgeeks.org/python-convert-a-list-of-tuples-into-dictionary/
+
+def TupleDict(tups):
+    res = {}
+    for a, b in tups:
+        res.setdefault(a, []).append(b)
+    return res
+
+if __name__ == "__main__":
+    tups = [("akash", 10), ("gaurav", 12), ("anand", 14), ("suraj", 20), ("akhil", 25), ("ashish", 30)]
+    print ("Results after converting : ", TupleDict(tups))
