57533fed273ea8606f644cb9fa5ea30689566765
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-03 08:16:47 -0800

    Basic - Sets - Mximum and minimum in a set

---
+++ python/practice/start_again/2024/11132024/maximum_and_minimum_in_a_set.py
@@ -0,0 +1,5 @@
+#   https://www.geeksforgeeks.org/python-maximum-minimum-set/
+
+sets = set([8, 16, 24, 1, 25, 3, 10, 65, 55])
+print ("Max from sets : ", max(sets))
+print ("Min from sets : ", min(sets))
