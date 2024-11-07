b9732f81a6d71238f2665b7ad432f36fd5fadf4f
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-07 11:30:14 -0800

    Basic - Sets - Convert sets to string

---
+++ python/practice/start_again/2024/11192024/convert_sets_to_string.py
@@ -0,0 +1,10 @@
+#   https://www.geeksforgeeks.org/convert-set-to-string-in-python/
+
+s = {'g', 'e', 'e', 'k', 's'}
+print ("Class Type : ", type(s))
+print ("Details after converting sets to string : ", s)
+
+s1 = str(s)
+print ("Class Type : ", type(s1))
+print ("Details after converting  String to sets : ", s1)
+
