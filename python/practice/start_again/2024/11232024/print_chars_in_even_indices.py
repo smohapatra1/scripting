cec02b00eedee330c57673eff9c1dfe941dde48a
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-11 20:45:40 -0800

    Random - Printing odd and even chars indices in a string

---
+++ python/practice/start_again/2024/11232024/print_chars_in_even_indices.py
@@ -0,0 +1,4 @@
+#   https://www.geeksforgeeks.org/problems/for-loop-2-python/1
+s = "DoctorPhenomenal"
+print ("Even Chars : ", s[::2]) # Printing Even Chars
+print ("Even Chars : ", s[1::2]) # Printing Odd Chars
