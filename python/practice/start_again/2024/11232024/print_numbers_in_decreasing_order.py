0c5b8b781246faacc1ed4cb40d0075a3df70905d
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-11 22:01:38 -0800

    Random - Printing numbers in decreasing order

---
+++ python/practice/start_again/2024/11232024/print_numbers_in_decreasing_order.py
@@ -0,0 +1,9 @@
+#   https://www.geeksforgeeks.org/problems/while-loop-in-python/1
+
+def ReversedOrder(x):
+    while x >= 0:
+        print (x)
+        x -= 1
+if __name__ == "__main__":
+    x = 3
+    ReversedOrder(x)
