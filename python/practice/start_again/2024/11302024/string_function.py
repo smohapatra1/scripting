d9a68492bd1fade3f2e6b8df8a02595d92c4769a
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-18 10:31:13 -0800

    Random - String fucntions

---
+++ python/practice/start_again/2024/11302024/string_function.py
@@ -0,0 +1,10 @@
+#   https://www.geeksforgeeks.org/problems/string-functions-i/1
+#   This is a function problem. Do not take any input. just Complete the functions 
+# trim(), exists(), titleIt(), casesSwap().
+# from collections import trim
+str = 'hello'
+print ("After Trimming =", str.strip('h'))
+s = 'll'
+if s in str:
+    print ("Sub string exists")
+print ("caseSwap Function  ", str.swapcase())
