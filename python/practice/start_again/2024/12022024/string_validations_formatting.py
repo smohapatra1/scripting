f9109f8d54f0142166af29f80d9444b1a7163580
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-19 10:12:55 -0800

    Basic - string validations and formatting

---
+++ python/practice/start_again/2024/12022024/string_validations_formatting.py
@@ -0,0 +1,9 @@
+# #   https://www.geeksforgeeks.org/problems/welcome-aboard-python/1
+
+# This module talks about Strings in Python. String in Python is immutable (cannot be edited). You have learnt about separators in Python. Let's start String with first question given below:
+
+# Given name of a person, the task is to welcome the person by printing the name with "Welcome". If name is "John", you should print "Welcome John".
+
+s = 'Samar'
+print ("Welcome " + s)
+print ("Welcome " , s)
