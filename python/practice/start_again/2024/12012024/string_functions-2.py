f095670dce5b4eddf282129a8d2b75d3840ef546
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-19 09:49:43 -0800

    Basic - String functions2

---
+++ python/practice/start_again/2024/12012024/string_functions-2.py
@@ -0,0 +1,6 @@
+#   https://www.geeksforgeeks.org/problems/string-functions-ii/1
+s = "gFgabcdEGfG"
+print ("String lower - ", s.lower())
+print ("String Upper - ", s.upper())
+print ("String starts with - ", s.startswith('g'))
+print ("String ends with - ", s.endswith('g'))
