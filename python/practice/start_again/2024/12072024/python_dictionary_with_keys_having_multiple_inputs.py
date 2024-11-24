c14318bc424013d233679b58afd0d176501ccbf8
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-24 10:18:52 -0800

    Basic - Dictionaries keys with multiple inputs

---
+++ python/practice/start_again/2024/12072024/python_dictionary_with_keys_having_multiple_inputs.py
@@ -0,0 +1,7 @@
+#   https://www.geeksforgeeks.org/python-dictionary-with-keys-having-multiple-inputs/
+dict = {}
+x, y, z = 10, 20, 30 
+dict[x, y, z] = x + y - z
+x, y, z = 5, 2, 4 
+dict[x, y, z] = x + y - z
+print (dict)
