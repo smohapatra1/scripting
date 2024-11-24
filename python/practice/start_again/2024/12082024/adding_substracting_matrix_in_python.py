da33cb9d9501bc9f5a7841db431d3ef12b12ddf4
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-24 10:39:55 -0800

    Basic - Add and Substract of matrix

---
+++ python/practice/start_again/2024/12082024/adding_substracting_matrix_in_python.py
@@ -0,0 +1,7 @@
+#   https://www.geeksforgeeks.org/adding-and-subtracting-matrices-in-python/
+import numpy as np
+
+A = np.array([[1,2],[3,4]])
+B = np.array([[4,5],[6,7]])
+print ("Addition ", np.add(A, B))
+print ("Sub ", np.subtract(A, B))
