a6bae6d2455907d4a55d75102d807b08c93a79fb
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-28 13:28:54 -0800

    Basic - Print multiple arguments

---
+++ python/practice/start_again/2024/12132024/print_multiple_arguments.py
@@ -0,0 +1,7 @@
+#   https://www.geeksforgeeks.org/how-to-print-multiple-arguments-in-python/
+
+def GfG(name, num):
+    print ("Hello from ", name + ", " + num)
+    print ("Hello %s and I am %s years old" %(name, num ))
+
+GfG("Sam ", "25")
