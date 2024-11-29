54176db355787ea8b40a852bb552f0fa0e7f75d2
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-29 15:02:04 -0800

    Basic - Find power of a number using recursion

---
+++ python/practice/start_again/2024/12142024/find_power_of_a_number_using_recursion.py
@@ -0,0 +1,13 @@
+# https://www.geeksforgeeks.org/python-program-to-find-the-power-of-a-number-using-recursion/
+
+def Power(N, P):
+    if P == 0:
+        return 1 
+    # return pow(N, P)
+    return (N * power(N, P-1))
+
+
+if __name__ == "__main__":
+    N = 2 
+    P = 3 
+    print (" ",Power(N, P ))
