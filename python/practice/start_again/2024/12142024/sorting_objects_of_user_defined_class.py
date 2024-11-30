cdadbad1d95a59235b800fbfb612173cdb6d5e34
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-11-30 08:30:58 -0800

    Basic - Fixed the error

--- python/practice/start_again/2024/12142024/sorting_objects_of_user_defined_class.py
+++ python/practice/start_again/2024/12142024/sorting_objects_of_user_defined_class.py
@@ -1,4 +1,4 @@
 #   https://www.geeksforgeeks.org/sorting-objects-of-user-defined-class-in-python/
 
 print (sorted([1, 9, 100, 20]))
-print (sorted("Geeks for gEEKS".spplt(), key=str.lower()))
+print (sorted("Geeks for gEEKS".split(), key=str.lower))
