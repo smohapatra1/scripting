e380e10b2ca2e2ea65cbf1138d7fe7c3b2b00229
smohapatra1 <samarendra.mohapatra121@gmail.com>
2024-12-30 10:56:26 -0800

    Linkedlist - Find middle of a linked list

---
+++ python/practice/start_again/2025/01072025/find_middle_of_a_linked_list_using_traversal.py
@@ -0,0 +1,68 @@
+#   https://www.geeksforgeeks.org/python-program-to-find-middle-of-a-linked-list-using-one-traversal/
+
+    # Python program for the above approach
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
+class NodeOperation:
+	# Function to add a new node
+	def pushNode(self, head_ref, data_val):
+
+		# Allocate node and put in the data
+		new_node = Node(data_val)
+
+		# Link the old list of the new node
+		new_node.next = head_ref
+
+		# move the head to point to the new node
+		head_ref = new_node
+		return head_ref
+
+	# A utility function to print a given linked list
+	def printNode(self, head):
+		while (head != None):
+			print('%d->' % head.data, end="")
+			head = head.next
+		print("NULL")
+
+	''' Utility Function to find length of linked list '''
+
+	def getLen(self, head):
+		temp = head
+		len = 0
+
+		while (temp != None):
+			len += 1
+			temp = temp.next
+
+		return len
+
+	def printMiddle(self, head):
+		if head != None:
+			# find length
+			len = self.getLen(head)
+			temp = head
+
+			# traverse till we reached half of length
+			midIdx = len // 2
+			while midIdx != 0:
+				temp = temp.next
+				midIdx -= 1
+
+			# temp will be storing middle element
+			print('The middle element is: ', temp.data)
+
+
+# Driver Code
+head = None
+temp = NodeOperation()
+head = temp.pushNode(head, 5)
+head = temp.pushNode(head, 4)
+head = temp.pushNode(head, 3)
+head = temp.pushNode(head, 2)
+head = temp.pushNode(head, 1)
+temp.printNode(head)
+temp.printMiddle(head)
