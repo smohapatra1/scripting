3340435fa6fe1b5dd6be03b5543a7893f540ded4
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-01-15 09:57:53 -0800

    Searching - insertion sort

---
+++ python/practice/start_again/2025/01242025/insertion_sort.py
@@ -0,0 +1,20 @@
+#   https://www.geeksforgeeks.org/python-program-for-insertion-sort/?ref=lbp
+
+def insertionSort(arr):
+    n = len(arr)
+    if n <= 1:
+        return
+    for i in range(1, n ):
+        key = arr[i]
+        j = i - 1 
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key 
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6]
+    # arr = [1]
+    print ("unsorted: ", arr)
+    insertionSort(arr)
+    print ("Sorted  :", arr)
