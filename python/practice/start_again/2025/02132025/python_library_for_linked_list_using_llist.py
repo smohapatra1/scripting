9a15d864327f6ab7a8e07895a1d4853021005b98
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-02-03 10:26:31 -0800

    DSA - Linked list using llist module

---
+++ python/practice/start_again/2025/02132025/python_library_for_linked_list_using_llist.py
@@ -0,0 +1,43 @@
+#   https://www.geeksforgeeks.org/python-library-for-linked-list/
+
+
+import llist
+from llist import sllist, sllistnode
+
+lst = sllist(['first', 'second', 'third'])
+print (lst)
+print (lst.first)
+print (lst.last)
+print(lst.size) 
+print() 
+  
+# adding and inserting values 
+lst.append('fourth') 
+node = lst.nodeat(2) 
+  
+lst.insertafter('fifth',node) 
+  
+print(lst) 
+print(lst.first) 
+print(lst.last) 
+print(lst.size) 
+print() 
+  
+# popping a value 
+#i.e. removing the last entry  
+# of the list 
+lst.pop() 
+print(lst) 
+print(lst.first) 
+print(lst.last) 
+print(lst.size) 
+print() 
+  
+# removing a specific element 
+node = lst.nodeat(1) 
+lst.remove(node) 
+print(lst) 
+print(lst.first) 
+print(lst.last) 
+print(lst.size) 
+print()
