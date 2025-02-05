d875718982a6325f01c253611f61e587a9958766
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-02-05 11:25:52 -0800

    DSA - Reverse contents of a file to a new file by lines

---
+++ python/practice/start_again/2025/02152025/reverse_the_content_of_a_file_and_store_in_another_file_word_to_word.py
@@ -0,0 +1,8 @@
+#   https://www.geeksforgeeks.org/python-program-to-reverse-the-content-of-a-file-and-store-it-in-another-file/
+
+f1 = open("output1.txt", "w")
+with open("file.txt", "r") as myfile:
+    data = myfile.readlines()
+data1 = data[::-1]
+f1.writelines(data1)
+f1.close() 
