d4c61fde5611c5132774a854b5f2342a52214488
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-02-19 11:09:10 -0800

    File write - Write dictionary to a file using loads and dumps

---
+++ python/practice/start_again/2025/03032025/write_dictionary_to_file_using_loads_dumps.py
@@ -0,0 +1,6 @@
+#   https://www.geeksforgeeks.org/write-a-dictionary-to-a-file-in-python/
+
+details = {'Name' : 'Bob', 'Age' : 28}
+
+with open('file.txt', 'w') as data:
+    data.write(str(details))
