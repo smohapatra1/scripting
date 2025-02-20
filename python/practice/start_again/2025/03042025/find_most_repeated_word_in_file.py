e780683f77d6f3b718cacc498e90a01c51764783
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-02-20 11:44:39 -0800

    File - find repeated words in a file

---
+++ python/practice/start_again/2025/03042025/find_most_repeated_word_in_file.py
@@ -0,0 +1,23 @@
+#   https://www.geeksforgeeks.org/find-the-most-repeated-word-in-a-text-file/
+
+
+file = open('file.txt', 'r')
+freq_word = ""
+freq = 0
+words = []
+
+for line in file:
+    line_word = line.lower().replace(',','').replace('.','').split(" ")
+    for w in line_word:
+        words.append(w)
+for i in range(0, len(words)):
+    count = 1
+    for j in range(i+1, len(words)):
+        if (words[i] == words[j]):
+            count = count +1
+    if count > freq:
+        freq = count
+        freq_word = words[i]
+print ("Most repeated words " + freq_word)
+print ("Frequency : " + str(freq))
+file.close()
