59b862a6ae00dae037b0136dc3ccbb6ee5964b99
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-02-26 10:45:09 -0800

    File - Read file words by word

---
+++ python/practice/start_again/2025/03112025/read_file_word_by_word.py
@@ -0,0 +1,6 @@
+#   https://www.geeksforgeeks.org/python-program-to-read-file-word-by-word/
+
+with open('file.txt', 'r') as f:
+    for line in f:
+        for word in line.split():
+            print (word)
