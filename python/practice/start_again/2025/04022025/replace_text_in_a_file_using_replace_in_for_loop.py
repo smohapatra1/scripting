7f7d59b8a986b6f08c535560046dae228f851032
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-03-18 12:52:52 -0700

    File - replace texts in files

---
+++ python/practice/start_again/2025/04022025/replace_text_in_a_file_using_replace_in_for_loop.py
@@ -0,0 +1,10 @@
+#   https://www.geeksforgeeks.org/python-program-to-replace-text-in-a-file/
+
+x = input("enter text to be replaced:")
+y = input("enter text that will replace:")
+with open('file.txt', 'r') as file:
+    filedata = file.read()
+    filedata = filedata.replace(x, y)
+with open('file.txt', 'w') as file:
+    file.write(filedata)
+print("data replaced successfully")
