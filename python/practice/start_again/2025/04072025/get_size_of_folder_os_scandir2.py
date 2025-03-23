d92a1f434977ec3e3f2d7482819c33cb4e5710e9
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-03-23 13:33:54 -0700

    files - get size of a folder using scandir with osstat

---
+++ python/practice/start_again/2025/04072025/get_size_of_folder_os_scandir2.py
@@ -0,0 +1,8 @@
+#   https://www.geeksforgeeks.org/how-to-get-size-of-folder-using-python/
+
+import os 
+size = 0 
+dir = 'test'
+for ele in os.scandir(dir):
+    size +=os.stat(ele).st_size
+print (size)
