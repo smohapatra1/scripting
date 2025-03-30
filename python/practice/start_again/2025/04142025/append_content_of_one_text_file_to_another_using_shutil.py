90f2f75595015ab6f1bed61b05c3ec13ced82cca
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-03-30 08:33:51 -0700

    files - append file content from one file to another using shutils

---
+++ python/practice/start_again/2025/04142025/append_content_of_one_text_file_to_another_using_shutil.py
@@ -0,0 +1,10 @@
+#   https://www.geeksforgeeks.org/python-append-content-of-one-text-file-to-another/
+
+import shutil
+
+def append_files(file1, file2):
+    with open(file.txt, 'r') as f1:
+        with open(file3.txt, '1') as f2:
+            shutil.copyfile(f1, f2)
+
+append_files(file1, file2)
