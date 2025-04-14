e2e9e2903beb650eca886dfc5389bfaf4fa766d2
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-04-14 11:03:43 -0700

    files - change cases for all chars in a txt file

---
+++ python/practice/start_again/2025/05012025/change_case_of_all_chars_in_a_txt_file.py
@@ -0,0 +1,6 @@
+#   https://www.geeksforgeeks.org/change-case-of-all-characters-in-a-txt-file-using-python/
+
+with open('file.txt', 'r') as f:
+    with open('file2.txt', 'a') as f2:
+        for line in f:
+            f2.write(line.upper())
