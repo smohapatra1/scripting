99c7f541c0fb173200bc7f0b501b2e33c421bb97
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-05-01 12:48:55 -0700

    json - json dumps type3

---
+++ python/practice/start_again/2025/05182025/json_dumps_3.py
@@ -0,0 +1,11 @@
+#   https://www.geeksforgeeks.org/json-dumps-in-python/
+
+import json
+
+Dictionary ={(1, 2, 3):'Welcome', 2:'to',
+            3:'Geeks', 4:'for',
+            5:'Geeks', 6:float('nan')}
+
+json_string = json.dumps(Dictionary, skipkeys = True, allow_nan = True)
+
+print('Equivalent json string of dictionary:', json_string)
