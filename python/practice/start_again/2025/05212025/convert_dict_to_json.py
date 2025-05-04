88436437dd9d1a54dd4d5aa9d3a57d3c7b7f62f6
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-05-04 15:30:33 -0700

    json - read write josn3

---
+++ python/practice/start_again/2025/05212025/convert_dict_to_json.py
@@ -0,0 +1,10 @@
+#   https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/
+
+import json
+dict = {
+    "id": "04",
+  "name": "sam",
+  "department": "HR"
+}
+json.object = json.dumps(dict, indent = 4  )
+print (json.object)
