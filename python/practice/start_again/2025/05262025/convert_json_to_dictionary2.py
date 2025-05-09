9dd0537ab69e0c38bead1d1b53949e45022ae6af
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-05-09 13:54:04 -0700

    json - convert json to dictionary2

---
+++ python/practice/start_again/2025/05262025/convert_json_to_dictionary2.py
@@ -0,0 +1,7 @@
+#   https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/
+
+import json
+with open('file.json') as f:
+    data = json.load(f)
+    print ("Type:" , type(data))
+    print ("\nemail:", data['email'])
