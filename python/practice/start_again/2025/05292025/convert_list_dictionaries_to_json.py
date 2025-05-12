0952384ce47df15fff4d02e9e290d94ca4dffc2c
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-05-12 12:53:03 -0700

    json - convert list of dictionaries to json

---
+++ python/practice/start_again/2025/05292025/convert_list_dictionaries_to_json.py
@@ -0,0 +1,18 @@
+#   https://www.geeksforgeeks.org/python-convert-list-of-dictionaries-to-json/
+
+
+import json
+
+data = [{"id": ("1", "2", "3"), "name": ("bhanu", "sivanagulu"),
+         "department": ("HR", "IT")},
+        {"id": ("4", "5", "6"), "name": ("sai", "poori"),
+         "department": ("HR", "IT")},
+        {"id": ("7", "8", "9"), "name": ("teja", "gowtam"),
+         "department": ("finance", "IT")},
+        {"id": ("10", "11", "12"), "name": ("sai", "jyothi"),
+         "department": ("business", "IT")},
+        {"id": ("13", "14", "15"), "name": ("prudhvi", "nagendram"),
+         "department": ("business", "IT")}]
+
+final = json.dumps(data, indent=2)
+print (final)
