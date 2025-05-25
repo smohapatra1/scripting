d928114579c526302665dd0ac79497dddcb9193e
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-05-25 08:29:39 -0700

    json - datetime module

---
+++ python/practice/start_again/2025/06112025/date_module.py
@@ -0,0 +1,7 @@
+#   https://www.geeksforgeeks.org/built-in-modules-in-python/
+
+import datetime
+date_time_today = datetime.date.today()
+print (date_time_today)
+date_time_now = datetime.datetime.now().time()
+print (date_time_now)
