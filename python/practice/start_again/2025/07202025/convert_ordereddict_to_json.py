344813d2e44279ae00fa391725f680427c4756a7
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-07-20 12:47:43 -0700

    Json - ordered dict to json

---
+++ python/practice/start_again/2025/07202025/convert_ordereddict_to_json.py
@@ -0,0 +1,7 @@
+#   https://www.geeksforgeeks.org/python/how-to-convert-ordereddict-to-json/
+
+from collections import OrderedDict
+
+od1 = OrderedDict([('1', 'one'), ('2', 'two')])
+print (type(od1))
+print (od1)
