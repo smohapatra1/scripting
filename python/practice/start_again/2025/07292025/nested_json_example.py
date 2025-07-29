21b7dc0e4be16a67f24619cc9eb6ce22264b24b8
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-07-29 11:21:19 -0700

    Json - Nested json example

---
+++ python/practice/start_again/2025/07292025/nested_json_example.py
@@ -0,0 +1,4 @@
+#   https://www.geeksforgeeks.org/python/difference-between-json-and-dictionary-in-python/
+
+nested_json = '{"person": {"name": "sam", "age": "21", "address" : "san"}}'
+print ("Nested Json string: ", nested_json)
