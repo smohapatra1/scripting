4693015944b679c6bfceadfb62e18d10710e6acb
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-08-29 10:35:57 -0700

    Json - serialize sqlalchemy result to json using jsonfy

---
+++ python/practice/start_again/2025/08292025/searialize_sqlalchemy_result_to_json_with_jsonfy.py
@@ -0,0 +1,17 @@
+#   https://www.geeksforgeeks.org/python/serialize-python-sqlalchemy-result-to-json/
+
+from sqlalchemy.ext.declarative import DeclarativeMeta
+import json
+class AlchemyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data)
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            return fields
+        return json.JSONEncoder.default(self, obj) 
