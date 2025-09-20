0cd775f2c6f1b5cccb8331ec352eae07b37d6be1
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-09-20 10:03:22 -0700

    Json - Convert bytes array into json using decode and io

---
+++ python/practice/start_again/2025/09202025/convert_bytes_array_into_json_using_decode_io_module.py
@@ -0,0 +1,12 @@
+#   https://www.geeksforgeeks.org/python/convert-a-bytes-array-into-json-format-in-python/
+
+import json
+import io
+byte = b'[{\'Date\': \'2016-05-21T21:35:40Z\', \'CreationDate\': \'2012-05-05\', \
+    \'LogoType\': \'png\', \'Ref\': 164611595, \'Classe\': [\'Email addresses\',\
+    \'Passwords\'],\'Link\':\'http://some_link.com\'}]'
+print (type(byte))
+fix_byte = byte.replace(b"'", b'"')
+my_json = json.load(io.BytesIO(fix_byte))
+print (my_json)
+print(type(my_json))
