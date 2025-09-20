5fdd3bb784db64d63b54c0eca2051eefe7faf99b
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-09-19 23:57:00 -0700

    Json - Convert bytes array into json using decode

---
+++ python/practice/start_again/2025/09192025/convert_bytes_array_into_json_using_decode.py
@@ -0,0 +1,8 @@
+#   https://www.geeksforgeeks.org/python/convert-a-bytes-array-into-json-format-in-python/
+
+import json
+bytes_data = b'{"key"": "value"", "nested": {"inner_key": [1, 2, 3]}}'
+print(type(bytes_data))
+json_data = json.loads(str(bytes_data, 'utf-8'))
+print (json_data)
+print(type(json_data))
