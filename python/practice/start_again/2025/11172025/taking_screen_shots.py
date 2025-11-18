bc0682e7896fee6e0782b4f0a0927e4fbf079bcc
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-11-17 22:46:42 -0800

    taking screen shots

---
+++ python/practice/start_again/2025/11172025/taking_screen_shots.py
@@ -0,0 +1,8 @@
+#   https://www.geeksforgeeks.org/python/taking-screenshots-using-pyscreenshot-in-python/
+
+import pyscreenshot
+image = pyscreenshot.grab()
+image.show()
+image.save(shot1.png)
+
+
