b45454c0ba02fafe7c74f4cbf50ced9174ca4493
smohapatra1 <samarendra.mohapatra121@gmail.com>
2025-12-02 23:11:58 -0800

    Fun factor generator webapp

---
+++ python/practice/start_again/2025/12022025/fun_fact_generator_web_app.py
@@ -0,0 +1,36 @@
+#   https://www.geeksforgeeks.org/python/fun-fact-generator-web-app-in-python/
+
+import json
+import requests
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+
+def get_fun_fact(_):
+    clear()
+    put_html(
+        '<p align="left">'
+        '<h2><img src="https://media.geeksforgeeks.org/wp-content/uploads/20210720224119/MessagingHappyicon.png" width="7%"> Fun Fact Generator</h2>'
+        '</p>'
+    )
+    url = "https://uselessfacts.jsph.pl/random.json?language=en"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    useless_fact = data['text']
+    style(put_text(useless_fact), 'color:blue; font-size: 30px')
+    put_buttons(
+        [dict(label='Click me', value='outline-success', color='outline-success')],
+        onclick=get_fun_fact
+    )
+if __name__ == '__main__':
+    # Put a heading "Fun Fact Generator"
+    put_html(
+        '<p align="left">'
+        '<h2><img src="https://media.geeksforgeeks.org/wp-content/uploads/20210720224119/MessagingHappyicon.png" width="7%"> Fun Fact Generator</h2>'
+        '</p>'
+    )
+    put_buttons(
+        [dict(label='Click me', value='outline-success', color='outline-success')],
+        onclick=get_fun_fact
+    )
+    hold()
